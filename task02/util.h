dfc90b0f260bcfb4d99857df674798f4b4f3b27b
yanaaa-it <yaninalikemiki@gmail.com>
2026-05-05 18:13:03 +0300

    Update util.h

--- task02/util.h
+++ task02/util.h
@@ -1,10 +1,12 @@
 #pragma once
 #include<iostream>
 #include<string>
+
 using namespace std;
-void print(string msg);
-void init_random(int** matrix, int size,int a,int b);
+
+void print(string s);
+void init_rnd(int** matrix, int size, int a, int b);
 void init_user(int** matrix, int size);
-int** create(int** matrix, int size);
-void remove(int** matrix, int size);
 string convert(int** matrix, int size);
+int** create(int size);
+void remove(int** matrix, int size);
